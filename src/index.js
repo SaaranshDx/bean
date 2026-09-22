53d32ef4dbec8b8e84c5cd4e183ac4159e7b2da1
SaaranshDx <saaransh762@gmail.com>
2026-09-22 16:45:16 +0530

    fix: fixed requests from other origins failing

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 app.use(apiRouter);
 
 app.get('/', (_req, res) => {
