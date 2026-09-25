92ab4a831b7fe44fb4dc37ada7c5c763117075ff
SaaranshDx <saaransh762@gmail.com>
2026-09-25 19:38:06 +0530

    undefined

--- src/index.js
+++ src/index.js
@@ -13,13 +13,6 @@ app.use(cors({
     origin: "https://saaranshdx.github.io"
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') return res.sendStatus(204);
-  next();
-});
 
 app.use(apiRouter);
 
